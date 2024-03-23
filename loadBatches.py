ca8bbad3082675a851ae4ce5742c8f4632eb0118
NikashPrakash <nikprakash20@gmail.com>
2024-03-23 19:37:17 -0400

    Load all eeg participants

--- loadBatches.py
+++ loadBatches.py
@@ -12,12 +12,14 @@ import math
 fpath = '/scratch/eecs448w24_class_root/eecs448w24_class/shared_data/brainWiz/EEG_data/'
 out_path_mapping = fpath + "chunks/mapping.pt"
 batchSize = 32
+obj_map = torch.tensor(pd.read_csv("/scratch/eecs448w24_class_root/eecs448w24_class/shared_data/brainWiz/Things_Images/THINGS/27 higher-level categories/category_mat_manual.tsv",delimiter="\t").values)
 
 def normalize_tensor(inp):
     inp = (inp - torch.mean(inp,1,keepdim=True)) / torch.std(inp,1,keepdim=True) #spatial
     return (inp - torch.mean(inp,0,keepdim=True)) / torch.std(inp,0,keepdim=True) #temporal
 
-for i in range(1, 2): #TODO CHANGE TO 51
+labels = []
+for i in range(1, 51): #TODO CHANGE TO 51
     sub_id = f"0{i}" if i < 10 else i
     eeg_file = fpath + f"sub-{sub_id}/eeg/sub-{sub_id}_task-rsvp_eeg.vhdr"
     tsv_file = fpath + f"sub-{sub_id}/eeg/sub-{sub_id}_task-rsvp_events.tsv"
@@ -37,7 +39,6 @@ for i in range(1, 2): #TODO CHANGE TO 51
     tensor_array = []
     tensors = []
     labels_array = []
-    labels = []
     unlabeled = []
     skipped = []
     task_f = pd.read_csv(tsv_file, delimiter="\t")
@@ -45,7 +46,6 @@ for i in range(1, 2): #TODO CHANGE TO 51
     startTimes = task_f["onset"]
     startTimes = startTimes.values
     objects_classes = objects_classes.values
-    obj_map = torch.tensor(pd.read_csv("/scratch/eecs448w24_class_root/eecs448w24_class/shared_data/brainWiz/Things_Images/THINGS/27 higher-level categories/category_mat_manual.tsv",delimiter="\t").values)
     j = 0 #note: full path given for debug purposes
     k = 0
     for i in startTimes:
@@ -53,7 +53,7 @@ for i in range(1, 2): #TODO CHANGE TO 51
         segment_data, _ = raw[:, start:start+segment_samples]  #type(segement_data) = ,
         segment_tensor = torch.tensor(segment_data,dtype=torch.float32)
         start += segment_samples
-        classification = obj_map[objects_classes[j]][[10,11,16]] #selecting rows 11, 17, 12
+        classification = obj_map[objects_classes[j]][[10,11,16]] #selecting cols 11, 12, 17
         if torch.sum(classification) == 1:
             if (k < batchSize):
                 tensor_array.append(normalize_tensor(segment_tensor))
@@ -75,14 +75,14 @@ for i in range(1, 2): #TODO CHANGE TO 51
         j += 1
 
     tensors = torch.stack(tensors).permute(0,1,3,2) #CURRENT IMPLEMENTATION OF CODE JUST GETS RID OF SAMPLES THAT DONT FIT INTO BATCHES OF SIZE BATCHSIZE
-    labels =  torch.stack(labels) #torch.vstack((mapping_sub1, torch.stack(labels))
     print("Shape of EEG tensor array:", tensors.shape)
-    print("Shape of Label tensor array:", labels.shape)
+    print(f"Shape of Label tensor array: {len(labels), len(labels[0]), len(labels[0][0])}")
     torch.save(tensors, out_path_eeg)
-    torch.save(labels, out_path_mapping)
     print("EEG Tensor Saved to " + out_path_eeg)
     print("Label tensor Saved to " + out_path_mapping)
     print("Skipped " + str(len(unlabeled)) + " unlabeled samples")
     print("Skipped " + str(len(skipped)) + " multilabeled samples")
-
+labels = torch.stack(labels)
+torch.save(labels, out_path_mapping)
+print(labels.shape)
 #TODO DOUBLE CHECK DATA
