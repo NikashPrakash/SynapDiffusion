81ed187188f2724ec69dfceeafd9100a89e4ba7a
NordiaG <nikprakash20@gmail.com>
2024-08-10 12:51:56 -0400

    structuring

--- models/__init__.py
+++
@@ -1,5 +0,0 @@
-from models.DWTDecoder import WaveletCNN
-from models.EEGDecoder import LSTM_CNN#, STGATE
-from models.MEGDecoder import MEGDecoder
-from models.FusionModels import FusionClassification
-
